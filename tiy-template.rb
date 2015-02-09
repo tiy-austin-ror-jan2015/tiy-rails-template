5ffd957ee9ac642a6b12ef8634e122c58fdae3ba
Jmingus <jmingus@austin.rr.com>
2015-02-09 13:36:50 -0600

    changed path in the uncomment_lines

--- tiy-template.rb
+++ tiy-template.rb
@@ -93,7 +93,7 @@ if get('Would you like to use Bourbon?')
 
   puts 'Uncommenting @import grid-settings'
 
-  uncomment_lines('app/assets/stylesheets/_base', (\w+@import))
+  uncomment_lines('app/assets/stylesheets/base/_base', (\w+@import))
 end
 
 
